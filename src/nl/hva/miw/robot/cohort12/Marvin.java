dc430db261a4d8ca999722bb2dbb63556242ab3f
Daan De rond <DaanDR@MacBook-Pro-van-Daan.local>
2018-06-11 16:22:06 +0200

    conflict in marvin

--- src/nl/hva/miw/robot/cohort12/Marvin.java
+++ src/nl/hva/miw/robot/cohort12/Marvin.java
@@ -5,7 +5,6 @@ import models.*;
 
 public class Marvin { 
 	 
-<<<<<<< HEAD
     public static void main(String[] args) {
    	//lijnvolger oproepen (voor parcour)
     LineFollower lijnvolger = new LineFollower(); 
@@ -15,28 +14,6 @@ public class Marvin {
 //   	
 //    DriveWithMusic muziekDemo = new DriveWithMusic(scale);
 //    muziekDemo.driveWithColor();
-=======
-    public static void main(String[] args)
-    {
-    
-    	LineFollower lijnvolger = new LineFollower();
-    	lijnvolger.followLine();
-    	
-    	//LineFollowerExtensive lijnvolger2 = new LineFollowerExtensive();
-    	//lijnvolger2.followLineextensive();
-    	
-//    	Music muziek = new Music("c");
-//    	muziek.makeMusic();
 
-    	
-//    	Scale scale = new Scale();
-//    		
-//    	
-//    	DriveWithMusic muziekDemo = new DriveWithMusic(scale);
-//    	muziekDemo.driveWithColor();
-    	
-
-    	
->>>>>>> infrared
     }
 }
